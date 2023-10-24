2fa20ab6686b447a2cf0262df4c4661cf3fe747c
Dean Patterson <deanp04@gmail.com>
2023-10-23 21:24:07 -0400

    reverseString

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,41 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
+// CHAINING BUILT IN METHODS
 
 function reverseString(text) {
-    // Code goes here
+    return [...text].reverse().join('')
 }
 
+module.exports = reverseString
 
+// // FOR-LOOP
+
+function reverseString(text) {
+    let result = '';
+    for(let char of text){
+        result = char + result
+    }
+    return result
+}
+
+module.exports = reverseString
+
+// RECURSION
+function reverseString(text) {
+    if(text === '') {
+        return ''
+    }else {
+        return reverseString(text.substr(1)) + text[0]
+    }
+}
+
+module.exports = reverseString
+
+// .reduce() (FASTEST)
+
+function reverseString(text) {
+    return text.split("").reduce((acc, c) => c + acc, '')
+}
 
 module.exports = reverseString
